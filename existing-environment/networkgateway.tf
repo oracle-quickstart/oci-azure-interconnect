983c11ed0c8fc95e24421858952dc7be5b13f3ce
Arun Poonia <poonia0arun@gmail.com>
2021-04-05 14:11:56 -0700

    fix: comment fast path config

--- existing-environment/networkgateway.tf
+++ existing-environment/networkgateway.tf
@@ -22,7 +22,8 @@ resource "azurerm_virtual_network_gateway_connection" "virtual_network_gateway_c
   name                         = var.virtual_network_gateway_connection_name
   location                     = data.azurerm_resource_group.resource_group_name.location
   resource_group_name          = var.resource_group_name
-  express_route_gateway_bypass = "true"
+  // Below commented line enables Fast Path on ExpressRoute, uncomment it if you need Fast Path (Additional Cost associated with this)
+  //express_route_gateway_bypass = "true"
   type                         = "ExpressRoute"
   virtual_network_gateway_id   = azurerm_virtual_network_gateway.virtual_network_gateway.id
   express_route_circuit_id     = azurerm_express_route_circuit.express_route_circuit.id
